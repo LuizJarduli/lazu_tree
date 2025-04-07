1c1fa342f71a5359087c01bf42ecf87560279979
Luiz Jarduli <luizjarduli@gmail.com>
2025-04-06 22:15:54 -0300

    feat: Convert App widget from Stateless to Stateful for enhanced state management

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:lazu_tree/app/app_router.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    GoRouter.optionURLReflectsImperativeAPIs = true;
+  }
 
   @override
   Widget build(BuildContext context) {
