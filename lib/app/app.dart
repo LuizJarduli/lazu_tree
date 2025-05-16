b1b92b2d9ac9f1dd795310ff03a0e78ea357bb09
Luiz Jarduli <luizjarduli@gmail.com>
2025-05-15 22:24:54 -0300

    feat(app): integrate BlocObserver and LoggerTalkerImpl for improved state management and logging

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lazu_tree/app/app_router.dart';
+import 'package:lazu_tree/app/core/bloc_observer/app_bloc_observer.dart';
+import 'package:lazu_tree/app/core/logger/logger_talker_impl.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -14,6 +17,8 @@ class _AppState extends State<App> {
   void initState() {
     super.initState();
     GoRouter.optionURLReflectsImperativeAPIs = true;
+
+    Bloc.observer = AppBlocObserver(LoggerTalkerImpl.getInstance());
   }
 
   @override
